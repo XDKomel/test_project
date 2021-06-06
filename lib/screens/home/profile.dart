b446633f3ecafabbf2d2e77ccd0281e8a7e61f9e
cosmos <mrskvayzer@gmail.com>
2021-06-06 05:17:30 +0300

    chat btn animation, other fixes

--- lib/screens/home/profile.dart
+++ lib/screens/home/profile.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:plane_chat/custom_widgets/rounded_input_field.dart';
 import 'package:plane_chat/models/SessionKeeper.dart';
 import 'package:plane_chat/screens/authentication/authenticate.dart';
 import 'package:plane_chat/services/auth.dart';
@@ -18,7 +20,7 @@ class _ProfileState extends State<Profile> {
   String email='';
 
   Future<String> getName() async {
-    DocumentReference documentReference =FirebaseFirestore.instance.collection('users').doc(SessionKeeper.user.uid);
+    DocumentReference documentReference =FirebaseFirestore.instance.collection('users').doc(FirebaseAuth.instance.currentUser!.uid);
     String name = '';
     await documentReference.get().then((snapshot) {
       name = snapshot.get('name');
@@ -26,7 +28,7 @@ class _ProfileState extends State<Profile> {
     return name;
   }
   Future<String> getEmail() async {
-    DocumentReference documentReference =FirebaseFirestore.instance.collection('users').doc(SessionKeeper.user.uid);
+    DocumentReference documentReference =FirebaseFirestore.instance.collection('users').doc(FirebaseAuth.instance.currentUser!.uid);
     String email = '';
     await documentReference.get().then((snapshot) {
       email = snapshot.get('email');
@@ -105,9 +107,9 @@ class _ProfileState extends State<Profile> {
                         mainAxisAlignment: MainAxisAlignment.center,
                         mainAxisSize: MainAxisSize.max,
                         children: <Widget>[
-                          Spacer(),
+                         // Spacer(),
                           SizedBox(
-                            height: 59,
+                            height: 34,
                           ),
                           Center(
                             child: Text(name,
@@ -131,183 +133,24 @@ class _ProfileState extends State<Profile> {
                           SizedBox(
                             height: 34,
                           ),
+
+                          // Container(
+                          //   margin: EdgeInsets.symmetric(horizontal: 23),
+                          //   child: RoundedInputField(
+                          //       initialValue: name,
+                          //       //textAlign: TextAlign.center,
+                          //       keyboard: TextInputType.text,
+                          //       width: 0.85,
+                          //       maxHeight: 0.07,
+                          //       maxCharacters: 30,
+                          //       onChanged: (name) {
+                          //         this.name = name;
+                          //       },
+                          // ),
+                          // ),
                           Spacer(),
                         ])))));
   }
 
-// @override
-// Widget build(BuildContext context) {
-//
-//   return Scaffold(
-//           appBar: PreferredSize(
-//             preferredSize: Size.fromHeight(constants.APPBAR_SIZE),
-//             child: AppBar(
-//               title: Text(
-//                 '',
-//                 style: TextStyle(color: Colors.white),
-//               ),
-//               backgroundColor: constants.accentColor,
-//               elevation: 0.0,
-//               leading: IconButton(
-//                 icon: Icon(Icons.arrow_back_ios),
-//                 onPressed: () {
-//                   Navigator.pop(context);
-//                 },
-//               ),
-//               actions: <Widget>[
-//                 TextButton(
-//                     onPressed: () async {
-//                       await _auth.signOut();
-//                       Navigator.popUntil(context, (route) => false);
-//                       Navigator.push(
-//                         context,
-//                         MaterialPageRoute(builder: (context) => Authenticate()),
-//                       );
-//                     },
-//                     child: Text(
-//                       constants.LOG_OUT,
-//                       style: TextStyle(
-//                           color: Colors.white,
-//                           fontSize: 15
-//                       ),
-//                     ))
-//               ],
-//             ),
-//           ),
-//       body: profileView()// This trailing comma makes auto-formatting nicer for build methods.
-//   );
-// }
-//
-// Widget profileView() {
-//   return Column(
-//     children: <Widget>[
-//       Padding(
-//         padding: EdgeInsets.fromLTRB(30, 50, 30, 30),
-//         child: Row(
-//           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//           children: <Widget>[
-//             Container(height: 50, width: 50 ,child: Icon(Icons.arrow_back_ios, size: 24,color: Colors.black54,), decoration: BoxDecoration(border: Border.all(color: Colors.black54), borderRadius: BorderRadius.all(Radius.circular(10))),),
-//             Text('Profiles details', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
-//             Container(height: 24,width: 24)
-//           ],
-//         ),
-//       ),
-//       Padding(
-//         padding: const EdgeInsets.fromLTRB(0, 0,0 ,50),
-//         child: Stack(
-//           children: <Widget>[
-//             CircleAvatar(
-//               radius: 70,
-//               child: ClipOval(child: Image.asset('assets/images/image1.png', height: 150, width: 150, fit: BoxFit.cover,),),
-//             ),
-//             Positioned(bottom: 1, right: 1 ,child: Container(
-//               height: 40, width: 40,
-//               child: Icon(Icons.add_a_photo, color: Colors.white,),
-//               decoration: BoxDecoration(
-//                   color: Colors.deepOrange,
-//                   borderRadius: BorderRadius.all(Radius.circular(20))
-//               ),
-//             ))
-//           ],
-//         ),
-//       ),
-//       Expanded(child: Container(
-//         decoration: BoxDecoration(
-//             borderRadius: BorderRadius.only(topLeft: Radius.circular(30), topRight: Radius.circular(30)),
-//             gradient: LinearGradient(
-//                 begin: Alignment.topRight,
-//                 end: Alignment.bottomLeft,
-//                 colors: [Color.fromRGBO(0, 41, 102, 1), constants.accentColor]
-//             )
-//         ),
-//         child: Column(
-//           children: <Widget>[
-//             Padding(
-//               padding: const EdgeInsets.fromLTRB(20, 25, 20, 4),
-//               child: Container(
-//                 height: 60,
-//                 child: Align(
-//                   alignment: Alignment.centerLeft,
-//                   child: Padding(
-//                     padding: const EdgeInsets.all(8.0),
-//                     child: TextFormField(
-//                       textAlign: TextAlign.start,
-//
-//                       textInputAction: TextInputAction.next,
-//
-//                       style: TextStyle(color: Colors.black),
-//
-//                       decoration: InputDecoration(
-//                         hintStyle: TextStyle(
-//                           fontWeight: FontWeight.normal,
-//                           color: Colors.grey,
-//                           fontSize: 18,
-//                         ),
-//
-//                         hintText: 'Name',
-//                         border: InputBorder.none,
-//                       ),
-//                     ),
-//                   ),
-//                 ), decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(20)),border: Border.all(width: 1.0, color: Colors.white70)),
-//               ),
-//             ),
-//             Padding(
-//               padding: const EdgeInsets.fromLTRB(20, 5, 20, 4),
-//               child: Container(
-//                 height: 60,
-//                 child: Align(
-//                   alignment: Alignment.centerLeft,
-//                   child: Padding(
-//                     padding: const EdgeInsets.all(8.0),
-//                     child: Text('Email', style: TextStyle(color: Colors.white70),),
-//                   ),
-//                 ), decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(20)),border: Border.all(width: 1.0, color: Colors.white70)),
-//               ),
-//             ),
-//             Padding(
-//               padding: const EdgeInsets.fromLTRB(20, 5, 20, 4),
-//               child: Container(
-//                 height: 60,
-//                 child: Align(
-//                   alignment: Alignment.centerLeft,
-//                   child: Padding(
-//                     padding: const EdgeInsets.all(8.0),
-//                     child: Text('Type something about yourself', style: TextStyle(color: Colors.white70),),
-//                   ),
-//                 ), decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(20)),border: Border.all(width: 1.0, color: Colors.white70)),
-//               ),
-//             ),
-//             Padding(
-//               padding: const EdgeInsets.fromLTRB(20, 5, 20, 4),
-//               child: Container(
-//                 height: 60,
-//                 child: Align(
-//                   alignment: Alignment.centerLeft,
-//                   child: Padding(
-//                     padding: const EdgeInsets.all(8.0),
-//                     child: Text('Phone number', style: TextStyle(color: Colors.white70),),
-//                   ),
-//                 ), decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(20)),border: Border.all(width: 1.0, color: Colors.white70)),
-//               ),
-//             ),
-//             Expanded(
-//               child: Align(
-//                 alignment: Alignment.bottomRight,
-//                 child: Container( height: 70, width: 200,
-//                   child: Align(child: Text('Save', style: TextStyle(color: Colors.white70, fontSize: 20),),),
-//                   decoration: BoxDecoration(
-//                       color: Colors.deepOrange,
-//                       borderRadius: BorderRadius.only(topLeft: Radius.circular(30),)
-//                   ),
-//                 ),
-//               ),
-//             )
-//           ],
-//         ),
-//       ))
-//     ],
-//   );
-// }
 
 }
